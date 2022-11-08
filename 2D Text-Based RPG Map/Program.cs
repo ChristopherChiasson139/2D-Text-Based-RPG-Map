e10347e5778b73dd7cee3d0b330bd87a5f1756ee
Christopher Chiasson <90638967+ChristopherChiasson139@users.noreply.github.com>
2022-11-08 09:34:53 -0400

    Extra Mile colour & debug

--- 2D Text-Based RPG Map/Program.cs
+++ 2D Text-Based RPG Map/Program.cs
@@ -12,51 +12,69 @@ namespace _2D_Text_Based_RPG_Map
         static void Main(string[] args)
         {
 
+            DisplayMap(2);
+            Console.ReadKey(true);
+            Console.Clear();
+            DisplayMap(3);
+            Console.ReadKey(true);
+            Console.Clear();
             DisplayMap(10);
-
+            Console.ReadKey(true);
+            Console.Clear();
+            DisplayMap(10000000);
             Console.ReadKey(true);
         }
         
         static void DisplayMap(int scale)
         {
+            //DEBUG
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("map scale = " + scale);
+            if (scale > 11)
+            {
+                scale = 11;
+            }
             //loop
             int y;
             int x;
-
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("");
             Console.Write("+");
             for (x = 0; x <= scale; x = x + 1)
             {
+
                 Console.Write("-");
-                
 
             }
             Console.Write("+");
             Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
             /////////////////////////////////////map
             for (y = 0; y <= scale; y = y + 1)
             {
-
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.Write("|");
                 for (x = 0; x <= scale; x = x + 1)
                 {
-                   
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.Write(map[y, x]);
 
                 }
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("|");
             }
             ///////////////////////////////////map
 
-            
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("+");
             for (x = 0; x <= scale; x = x + 1)
             {
-                Console.Write("-");
 
+                Console.Write("-");
 
             }
             Console.Write("+");
-
+            Console.ForegroundColor = ConsoleColor.White;
 
 
         }
@@ -80,5 +98,6 @@ namespace _2D_Text_Based_RPG_Map
         // ` = grass
         // ~ = water
         // * = trees
+
     }
 }
